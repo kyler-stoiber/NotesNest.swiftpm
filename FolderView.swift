cfb26e9f4a6225946b8488d32006e55d44fce34d
Tommy Rokicki <rok512515@d204.lths.net>
2025-05-01 11:46:27 -0500

    jalsdkgoqbg

---
+++ FolderView.swift
@@ -0,0 +1,18 @@
+//
+//  FolderView.swift
+//  NotesNest
+//
+//  Created by Rokicki, Thomas (512515) on 4/30/25.
+//
+
+import SwiftUI
+
+struct FolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FolderView()
+}
