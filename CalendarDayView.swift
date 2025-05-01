054ab26f38372870b373874060d842c8e4d2b00b
Kyler Stoiber <sto512144@d204.lths.net>
2025-05-01 11:46:16 -0500

    00

---
+++ CalendarDayView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarDayView.swift
+//  NotesNest
+//
+//  Created by Stoiber, Kyler (512144) on 4/30/25.
+//
+
+import SwiftUI
+
+struct CalendarDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarDayView()
+}
