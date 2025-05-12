e62e3dbc531f0907d5aa86c0c9cf381e46936005
Tommy Rokicki <rok512515@d204.lths.net>
2025-05-12 11:33:07 -0500

    hwjqadgj

--- SliderView.swift
+++ SliderView.swift
@@ -1,18 +1,18 @@
 //
-//  FolderView.swift
+//  SliderView.swift
 //  NotesNest
 //
-//  Created by Rokicki, Thomas (512515) on 4/30/25.
+//  Created by Rokicki, Thomas (512515) on 5/1/25.
 //
 
 import SwiftUI
 
-struct FolderView: View {
+struct SliderView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    FolderView()
+    SliderView()
 }
